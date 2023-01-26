db75eb900fd062549b08c0e5fc591c7cb10388a9
Isaac Rodriguez <isaac_rodriguez@live.com>
2023-01-25 20:57:13 -0500

    [CIFIX]: Fixes package name for coverage

--- boltfile.py
+++ boltfile.py
@@ -2,6 +2,8 @@ import os.path
 
 import bolt
 
+import pythern.about as about
+
 bolt.register_task('default', ['pip', 'ut'])
 bolt.register_task('clear-pyc', ['delete-pyc.source', 'delete-pyc.tests'])
 bolt.register_task('ut', ['clear-pyc', 'nose'])
@@ -13,7 +15,7 @@ bolt.register_task('run-unit-tests', ['clear-pyc', 'mkdir', 'nose.ci'])
 
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-SRC_DIR = os.path.join(PROJECT_ROOT, 'dichotomy')
+SRC_DIR = os.path.join(PROJECT_ROOT, about.project)
 TEST_DIR = os.path.join(PROJECT_ROOT, 'tests')
 OUTPUT_DIR = os.path.join(PROJECT_ROOT, 'output')
 COVERAGE_DIR = os.path.join(OUTPUT_DIR, 'coverage')
@@ -46,7 +48,7 @@ config = {
                 'xunit-file': os.path.join(OUTPUT_DIR, 'unit_tests_log.xml'),
                 'with-coverage': True,
                 'cover-erase': True,
-                'cover-package': 'dichotomy',
+                'cover-package': about.project,
                 'cover-html': True,
                 'cover-html-dir': COVERAGE_DIR,
                 'cover-branches': True,
